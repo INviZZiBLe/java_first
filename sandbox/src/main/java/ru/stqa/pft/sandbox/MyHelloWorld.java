a3dbc7903b7969e1912e4475de3056763306ece7
Aleksandr Polyakov <invizzible13@gmail.com>
2017-02-23 13:32:27 +0200

    Added some functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyHelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyHelloWorld.java
@@ -1,7 +1,32 @@
 package ru.stqa.pft.sandbox;
 
 public class MyHelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello,World!");
+        hello("World");
+        hello("User");
+        hello("Alexandr");
+
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со стопронами " + a + " и " + b + " = " + area(a,b));
+        }
+
+
+    public static void hello(String somebody) { // String somebody -  это параметр (или аргумент) функции hello
+        System.out.println("Hello, " + somebody + "!");
+
+    }
+
+    public static double area (double len){
+        return len * len;
+
+    }
+
+    public static double area (double a, double b){
+        return a * b;
     }
 }
