8e98d0e438b5d1476b335b180ab1a3774779a988
Aleksandr Polyakov <invizzible13@gmail.com>
2017-02-28 21:57:54 +0200

    My First Test

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -7,7 +7,11 @@ public class Square {
 
   public double l;
 
-  public Square (double len){
-    this.l = len;
+  public Square (double l){
+    this.l = l;
   }
+
+  public double area (Square s){ //дописал подсчет площади квадрата, так как без него не запускался тест
+    return this.l * l;
+}
 }
